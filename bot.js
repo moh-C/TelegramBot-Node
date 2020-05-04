aba535885f88f2893d9563f48b41cb0be8e69e14
Aaron <mohammad.chegini@hotmail.com>
2020-05-04 14:58:57 +0430

    use of middlewares

--- bot.js
+++ bot.js
@@ -3,9 +3,15 @@ const Telegraph = require('telegraf');
 const bot = new Telegraph('1224771643:AAHpQ1pU5YvDeKDh4_McnxxComFTQMswZ9k');
 
 // code
-bot.use(ctx => {
+bot.use((ctx, next) => {
+    ctx.state.apple = new Date();
     ctx.reply('You used the bot mofo!');
+    next(ctx);
 })
 
+bot.start(ctx => {
+    //ctx.reply('This is reply part');
+    ctx.reply(ctx.state.apple);
+})
 
 bot.launch();
