39c47fcbde03ef8332f75465e367d7946e40dc9d
Aaron <mohammad.chegini@hotmail.com>
2020-05-04 15:24:31 +0430

    End of part one

--- bot.js
+++ bot.js
@@ -4,14 +4,17 @@ const bot = new Telegraph('1224771643:AAHpQ1pU5YvDeKDh4_McnxxComFTQMswZ9k');
 
 // code
 bot.use((ctx, next) => {
-    ctx.state.apple = new Date();
-    ctx.reply('You used the bot mofo!');
+    //ctx.reply('You used the bot!');
     next(ctx);
 })
 
 bot.start(ctx => {
     //ctx.reply('This is reply part');
-    ctx.reply(ctx.state.apple);
+    //ctx.reply(ctx.state.apple);
+    bot.telegram.sendMessage(ctx.chat.id, 'Yo!', {
+        parse_mode: 'Markdown',
+        disable_notification: true
+    })
 })
 
 bot.launch();
