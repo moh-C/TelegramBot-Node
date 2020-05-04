6f0e4098972032240977f4805ab2f872db07f676
Aaron <mohammad.chegini@hotmail.com>
2020-05-04 14:41:51 +0430

    Bot.on method

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const Telegraph = require('telegraf');
 const bot = new Telegraph('1224771643:AAHpQ1pU5YvDeKDh4_McnxxComFTQMswZ9k');
 
 // code
-bot.start((ctx) => {
+/*bot.start((ctx) => {
     ctx.reply(`Holas ${ctx.from.first_name}!`);
 })
 
@@ -14,5 +14,23 @@ bot.help((ctx) => {
 bot.settings((ctx) => {
     ctx.reply('Settings?! Yummy!')
 })
+
+bot.command(["test", "tests", "Test"],(ctx)=>{
+    ctx.reply('Yo!');
+})*/
+
+bot.hears('Cat', (ctx)=>{
+    ctx.reply('Fuck you!');
+})
+
+bot.on('sticker', (ctx)=>{
+    ctx.reply('That was a sticker!');
+})
+
+bot.on('photo', (ctx)=>{
+    ctx.reply('That was a photo!');
+})
+
+
 
 bot.launch();
