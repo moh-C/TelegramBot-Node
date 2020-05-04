105d72ca175928c152289c00b70ebb3505387e5b
Aaron <mohammad.chegini@hotmail.com>
2020-05-04 14:47:26 +0430

    bot.use method for every request

--- bot.js
+++ bot.js
@@ -3,45 +3,9 @@ const Telegraph = require('telegraf');
 const bot = new Telegraph('1224771643:AAHpQ1pU5YvDeKDh4_McnxxComFTQMswZ9k');
 
 // code
-/*bot.start((ctx) => {
-    ctx.reply(`Holas ${ctx.from.first_name}!`);
+bot.use(ctx => {
+    ctx.reply('You used the bot mofo!');
 })
-
-bot.help((ctx) => {
-    ctx.reply('You entered help!')
-})
-
-bot.settings((ctx) => {
-    ctx.reply('Settings?! Yummy!')
-})
-
-bot.command(["test", "tests", "Test"],(ctx)=>{
-    ctx.reply('Yo!');
-})*/
-
-bot.hears('Cat', (ctx)=>{
-    ctx.reply('Fuck you!');
-});
-
-bot.on('sticker', (ctx)=>{
-    ctx.reply('That was a sticker!');
-});
-
-bot.on('photo', (ctx)=>{
-    ctx.reply('That was a photo!');
-});
-
-bot.mention('aaro_n', ctx => {
-    ctx.reply('You mentioned that piece of shit did ya?');
-})
-
-bot.hashtag('hash', ctx =>{
-    ctx.reply('Hashtags are here!');
-})
-
-bot.phone('9102168514', (ctx)=>{
-    ctx.reply('Yeah we get it you wanna phone that fucker!')
-});
 
 
 bot.launch();
