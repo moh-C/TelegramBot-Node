4c997bae7e344f408ad2a6a0b6ccef8a83ab129a
Aaron <mohammad.chegini@hotmail.com>
2020-05-04 16:01:17 +0430

    echobot halfway through

--- bot.js
+++ bot.js
@@ -1,20 +1,38 @@
 const Telegraph = require('telegraf');
 
-const bot = new Telegraph('1224771643:AAHpQ1pU5YvDeKDh4_McnxxComFTQMswZ9k');
+const bot = new Telegraph('863690395:AAGmIxp11JrN3DbuQxD9L0wnph1lgs6hbAM');
 
-// code
-bot.use((ctx, next) => {
-    //ctx.reply('You used the bot!');
-    next(ctx);
-})
+const helpMessage = `
+Welcome to help!
+Ask something from me and I'll do my best to help you!
+`;
 
 bot.start(ctx => {
-    //ctx.reply('This is reply part');
-    //ctx.reply(ctx.state.apple);
-    bot.telegram.sendMessage(ctx.chat.id, 'Yo!', {
-        parse_mode: 'Markdown',
-        disable_notification: true
-    })
+    logger(ctx);
+    ctx.reply('Welcome to this bot!');
 })
+
+bot.help(ctx => {
+    logger(ctx);
+    ctx.reply(helpMessage);
+})
+
+bot.command('echo', (ctx) => {
+    logger(ctx);
+    let input = ctx.message.text;
+    input = input.split(' ');
+    let finalMessage = 'You said echo!';
+    if(input.length != 1) {
+        input.shift();
+        finalMessage = input.join(' ');
+    }
+    ctx.reply(finalMessage);
+})
+
+let logger = (ctx) => {
+    let text = ctx.message.text;
+    let username = ctx.from.username;
+    console.log(username + ' said ' + text);
+}
 
 bot.launch();
