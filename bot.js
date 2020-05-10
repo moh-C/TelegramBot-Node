28c92c5275c32272fc91a8e3c2dbd1da6463fea8
Aaron <mohammad.chegini@hotmail.com>
2020-05-10 11:32:21 +0430

    multiple commands all at once

--- bot.js
+++ bot.js
@@ -2,16 +2,25 @@ const Telegraph = require('telegraf');
 
 const bot = new Telegraph('1139511873:AAFNoMjslfc0e0v9d0uhVSC_7iWoZg8ZLuQ'); 
 
-const axios = require('axios');
-const fs = require('fs');
+let stateof = '';
 
+bot.use((ctx, next) => {
+    let msg = '';
+    try {
+        let msg = ctx.message.text;
+    } catch(err) {
+        console.log(err);
+    }
+    console.log(msg);
+    next();
+})
 
-bot.command('test', ctx => {
+bot.command('start', ctx => {
     bot.telegram.sendMessage(ctx.chat.id, 'Welcome', {
         reply_markup: {
             inline_keyboard: [
                 [
-                    { text: 'One', callback_data: 'none' },
+                    { text: 'firstOne', callback_data: 'firstOne' },
                     { text: 'One', callback_data: 'none' }
                 ],
                 [
@@ -26,67 +35,20 @@ bot.command('test', ctx => {
     })
 })
 
-bot.command('fortune', ctx => {
-    axios.get('http://yerkee.com/api/fortune')
-        .then(res => {
-            ctx.reply(res.data.fortune);
-        })
-        .catch(e => console.log(e))
+let commands = ['97242042', '97242043'];
+
+bot.hears(commands, ctx => {
+    console.log(ctx.message.text);
 })
 
-bot.command('cat', async ctx => {
-    let input = ctx.message.text;
-    input = input.split(' ');
-    if (input.length == 1)
-    {
-        let res = await axios.get('http://aws.random.cat/meow');
-        ctx.replyWithPhoto(res.data.file);
-    }
-    else {
-        input.shift();
-        ctx.replyWithPhoto(`https://cataas.com/cat/says/${input.join(' ')}`)
-    }
+bot.action('firstOne', async ctx => {
+    ctx.answerCbQuery('Tnx');
+    stateof = 'firstOne';
+    bot.telegram.sendMessage(ctx.chat.id,'Enter the number: ');
 })
 
-bot.command('dogbreed', ctx => {
-    let rawdata = fs.readFileSync("./dogbreed.json", "utf-8");
-    let data = JSON.parse(rawdata);
-    let message = '';
-    data.forEach(element => {
-        message += ` - ${element}\n`
-    });
-    ctx.reply(message);
-})
-
-bot.command('dog', async ctx => {
-    let input = ctx.message.text;
-    input = input.split(' ');
-    
-    if (input.length == 1) {
-        return
-    }
-    input.shift();
-    let final = input.join(' ');
-    let rawdata = fs.readFileSync("./dogbreed.json", "utf-8");
-    let data = JSON.parse(rawdata);
-    
-    if (data.includes(final)) {
-        let res = await axios.get(`https://dog.ceo/api/breed/${final}/images/random`)
-        ctx.replyWithPhoto(res.data.message);
-    }
-    else {
-        let suggestions = data.filter( e => {
-            return e.startsWith(final)
-        })
-        if (suggestions.length) {
-            let message = 'Did you mean:\n';
-            suggestions.forEach(e => message += ` - ${e} \n`);
-            ctx.reply(message);
-        }
-        else ctx.reply('Sorry no dog breed starts with that!');
-    }
-
-
-})
+function log(ctx) {
+    console.log(ctx);
+}
 
 bot.launch();
